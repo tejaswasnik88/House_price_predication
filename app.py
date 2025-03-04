92bf7a6021ff86023173fab8e462465f46220cc2
Tejas Wasnik <tejaswasnik88@gmail.com>
2025-03-04 18:30:08 +0530

    add dataset in data folder

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 # from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
@@ -25,46 +24,11 @@ app = Flask(__name__)
 #         return f"<House {self.city}>"
 
 
-# @app.route("/")
-# def home():
-#     houses = House.query.all()
-#     return render_template("index.html", houses=houses)
+ 
 
-# # Route to update a house's details
-# @app.route("/update/<int:id>", methods=["POST"])
-# def update(id):
-#     house = House.query.get(id)
-#     if not house:
-#         return f"House with ID {id} not found", 404
+df=pd.read_csv("data/house_prices_india.csv")
 
-#     if request.method == "POST":
-#         # Update the house with new values from the form
-#         house.city = request.form['city']
-#         house.price = request.form['price']
-#         house.bedroom = request.form['bedroom']
-
-#         # Commit the changes to the database
-#         db.session.commit()
-
-#         return redirect(url_for('home'))  # Redirect to home page after update
-
-#     # If it's a GET request, render the update form
-#     # return render_template("update.html", house=house)
-
-# # Function to create the tables in the database
-# @app.before_request
-# def create_tables():
-#     db.create_all()
-
-# train data
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import LabelEncoder
-
-
-df=pd.read_csv("C:\\Users\\HP\\Downloads\\house_prices_india.csv")
+# df=pd.read_csv("C:\\Users\\HP\\Downloads\\house_prices_india.csv")
 # print(df)
 # x=df['City'].tolist()
 unique_cities = df["City"].unique()
@@ -94,7 +58,7 @@ model.fit(X_train,y_train)
 # pred=model.predict([[le.transform(["Delhi"])[0],2951,2,3]])
 
 
-
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
